43dd2c21806db1126e8a63eca11edba0931c7a3b
saadmanfarhad <saadmanheavy@gmail.com>
2018-11-04 11:41:54 +0600

    Completed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,9 +19,43 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  if(req.params.date_string){
+    var unixMili = new Date(parseInt(req.params.date_string));
+    var date = new Date(req.params.date_string);
+
+    if(unixMili == 'Invalid Date' && date == 'Invalid Date'){
+      res.json({
+        "unix": null,
+        "utc": "Invalid Date"
+      })
+    }
+    else{
+      if(date != 'Invalid Date'){
+        res.json({
+          "unix": Math.round(date.getTime()),
+          "utc": date.toUTCString()
+        })
+      }
+      else{
+        res.json({
+          "unix": Math.round(unixMili.getTime()),
+          "utc": unixMili.toUTCString()
+        })
+      }
+    }
+  }
+  else{
+    res.json({
+      "unix": Math.round((new Date()).getTime()),
+      "utc": new Date().toUTCString()
+    });
+  }
 });
 
 
